89bd8e995a69b9663703e873fe612aa0043d6946
Amanr4009 <aman.keshri652@gmail.com>
2024-05-13 13:20:16 +0530

    single page

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed =new Typed(".text",{
-    strings:["Fullstack Developer" , "Frontend Developer"],
+    strings:["Fullstack Developer" , "Software Developer"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
